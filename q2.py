01b99061d8b46e354497113fb2ea84a87ea7c063
gauravnetes <gouravchandra935@gmail.com>
2024-12-03 23:10:05 +0530

    initial commit

---
+++ q2.py
@@ -0,0 +1,9 @@
+# 2.	Finding the Largest Number in an Array
+# Method 1: Using the max () function
+
+import array
+
+arr = array.array('i', [2, 314, 42, 43, 5])
+
+largest = max(arr)
+print("The largest element in the array is:", largest)
