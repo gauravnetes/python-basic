2c8d08f091c5eb7d21564d16c97ff38b1b0d56df
gauravnetes <gouravchandra935@gmail.com>
2024-12-07 23:31:49 +0530

    initial commit

---
+++ q6.py
@@ -0,0 +1,7 @@
+# Summation of Two Numbers
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+sum_result = num1 + num2
+print(f"sum of {num1} and {num2} is {sum_result}")
