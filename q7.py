72f6073ef18e358f9e6cefa28a7886848eba81ca
gauravnetes <gouravchandra935@gmail.com>
2024-12-09 18:17:45 +0530

    initial commit

---
+++ q7.py
@@ -0,0 +1,7 @@
+# Program to check if the given number is odd or even
+
+num = int(input("Enter a num: "))
+if num % 2 == 0:
+    print(f"{num} is an even number")
+else: 
+    print(f"{num} is an odd number")
