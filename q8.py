ede04a60fc4daccb873c72f17c6f4f924d5559c4
gauravnetes <gouravchandra935@gmail.com>
2024-12-10 23:57:03 +0530

    initial commit

---
+++ q8.py
@@ -0,0 +1,15 @@
+# grading system
+marks = float(input("Enter your marks [1 - 100]: "))
+
+if marks >= 90:
+    print("Grade: A")
+elif marks >= 80:
+    print("Grade: B")
+elif marks >= 70:
+    print("Grade: C ")
+elif marks >= 60: 
+    print("Grade D")
+elif marks >= 50:
+    print("Grade: E")
+else:
+    print("FAIL")
