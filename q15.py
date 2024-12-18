e185a3f7cefa3b2a6f6d0a63b643c0cca2167f0a
gauravnetes <gouravchandra935@gmail.com>
2024-12-19 00:08:45 +0530

    initial commit

---
+++ q15.py
@@ -0,0 +1,9 @@
+a = 5
+b = 3
+
+print(f"a & b: {a & b}") # AND
+print(f"a | b: {a | b}") # OR
+print(f"a ^ b = {a ^ b}")  # XOR
+print(f"~a = {~a}")  # NOT
+print(f"a << 1: {a << 1}") # Left shift 101 => 1010
+print(f"a >> 1: {a >> 1}") # Right shift 101 => 0101
